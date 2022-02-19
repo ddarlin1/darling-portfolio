663a2b91183c5d2a6148b2517e1cc389d9e54ba6
darling_d <ddarling212@gmail.com>
2022-02-19 16:46:23 -0500

    Added contact submission form

--- js/script.js
+++ js/script.js
@@ -1,7 +1,21 @@
-// NAV MENU
+// nav menu
 const toggle = document.querySelector('.toggle');
 const nav = document.querySelector('.full-screen-nav');
 const backdrop = document.querySelector('.backdrop');
 
 toggle.addEventListener('click', () => nav.classList.add('open-nav'));
 backdrop.addEventListener('click', () => nav.classList.remove('open-nav'));
+
+// contact form - clear text upon submission
+const btn = document.querySelector('.btn');
+const messageForm = document.getElementById('message');
+
+btn.addEventListener('click', setTimeout);
+
+setTimeout(resetForm, 1000);
+
+console.log(messageForm.value);
+
+function resetForm() {
+    messageForm.value = '';
+}
