385e0e28848fb250aaa98f108e70112811a3d103
darling_d <ddarling212@gmail.com>
2022-02-23 19:16:37 -0500

    Coded scroll to top button

--- js/script.js
+++ js/script.js
@@ -1,3 +1,22 @@
+// scroll to top button
+const topButton = document.getElementById("scroll-button");
+
+window.onscroll = function() {scrollFunction()}; 
+
+function scrollFunction() {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        topButton.style.display = "block"; 
+    } else {
+        topButton.style.display = "none"; 
+    }
+}
+
+//When user clicks button, scroll to top of page
+function topFunction() {
+    document.body.scrollTop = 0; //For Safari
+    document.documentElement.scrollTop = 0; //For Chrome, Firefox, IE and Opera
+}
+
 // nav menu
 const toggle = document.querySelector('.toggle');
 const nav = document.querySelector('.full-screen-nav');
